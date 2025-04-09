27aa80d782379a0def8dcdc42edd8d556a360bcd
shweta-585 <sweta.app19@gmail.com>
2025-04-09 14:06:28 +0530

    add hex add code

--- calculator.sh
+++ calculator.sh
@@ -16,3 +16,22 @@ elif [ $choice == "div" ]; then
 else
 	echo "Invalid choice"
 fi
+
+read -p "Enter hex a: " ahex
+read -p "Enter hex b: " bhex
+a=$((ahex))
+b=$((bhex))
+
+read -p "Enter choice(hexadd/hexsub/hexmult/hexdiv): " choice
+
+if [ $choice == "hexadd" ]; then
+        printf "0x%X\n" $((a+b))
+elif [ $choice == "hexsub" ]; then
+        printf "0x%X\n" $((a-b))
+elif [ $choice == "hexmult" ]; then
+        printf "0x%X\n" $((a*b))
+elif [ $choice == "hexdiv" ]; then
+        printf "0x%X\n" $((a/b))
+else
+        echo "Invalid choice"
+fi
